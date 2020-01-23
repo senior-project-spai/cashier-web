5bb1b21c19d43f11a30e34522ed25033b1d940a1
Rawit Panjaroen <check.rawit@gmail.com>
2020-01-23 21:23:01 +0700

    feat: loading state & blank image & squared button

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { TextField, Button, Box } from "@material-ui/core";
-import { useAlert } from 'react-alert'
+import { TextField, Button, Box, CircularProgress } from "@material-ui/core";
+import { useAlert } from "react-alert";
+
+import SquaredButton from "./SquaredButton";
 
 function UserInput(props) {
-  const alert = useAlert()
+  const alert = useAlert();
+  const [isLoading, setIsLoading] = useState(false);
   const isValidUserID = userID => {
     return true;
   };
 
   const userSubmitHandler = async e => {
+    setIsLoading(true);
     e.preventDefault();
     const userInput = props.userID;
     let detection_response = null;
@@ -18,14 +22,16 @@ function UserInput(props) {
       try {
         // Enter RaspPi detection Link
         detection_response = await axios.get("http://127.0.0.1:8002/detection");
-      }
-      catch (error) {
+      } catch (error) {
         console.error(error);
         alert.show(error.message + " please try again");
       }
 
-      if (detection_response) { props.setUser(detection_response.data); }
+      if (detection_response) {
+        props.setUser(detection_response.data);
+      }
     }
+    setIsLoading(false);
   };
 
   return (
@@ -42,15 +48,19 @@ function UserInput(props) {
           autoFocus
           value={props.userID}
         />
-        <Button
+        <SquaredButton
           variant="contained"
           color="secondary"
           size="small"
           type="submit"
           disableElevation
         >
-          Enter
-        </Button>
+          {!isLoading ? (
+            "Enter"
+          ) : (
+            <CircularProgress size={32} color="inherit" />
+          )}
+        </SquaredButton>
       </Box>
     </form>
   );
